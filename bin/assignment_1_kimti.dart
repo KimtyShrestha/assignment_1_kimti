d1504de535069abd94c4ec48836169e1d843d71c
Kimty Shrestha <kimtyshrestha@gmail.com>
2025-11-08 12:56:16 +0545

    Added project folder structure and empty Dart files

--- bin/assignment_1_kimti.dart
+++
@@ -1,5 +0,0 @@
-import 'package:assignment_1_kimti/assignment_1_kimti.dart' as assignment_1_kimti;
-
-void main(List<String> arguments) {
-  print('Hello world: ${assignment_1_kimti.calculate()}!');
-}
--- test/assignment_1_kimti_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:assignment_1_kimti/assignment_1_kimti.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
