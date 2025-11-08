ea48e2e9aa3ba4cdcc8331c8250433d69bd54360
Kimti-Digi <kimti.shrestha@digischoolglobal.com>
2025-11-08 12:40:03 +0545

    Initial commit - Clean setup

---
+++ bin/assignment_1_kimti.dart
@@ -0,0 +1,5 @@
+import 'package:assignment_1_kimti/assignment_1_kimti.dart' as assignment_1_kimti;
+
+void main(List<String> arguments) {
+  print('Hello world: ${assignment_1_kimti.calculate()}!');
+}
---
+++ test/assignment_1_kimti_test.dart
@@ -0,0 +1,8 @@
+import 'package:assignment_1_kimti/assignment_1_kimti.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
